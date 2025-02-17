d06b384111c751d8717371492e1b0fbe61b9e375
blitzbrian <blitzbrianx@gmail.com>
2025-02-17 08:02:56 +0100

    Make a beginning

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ['cache.downloadroms.io'], // Add the external CDN domain here
+  },
 };
 
 export default nextConfig;
